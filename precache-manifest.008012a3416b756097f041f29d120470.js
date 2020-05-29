f35a88e09466d3c8b7a4300fa3b263272019804e
Anshuman301 <anshumanprsd@gmail.com>
2020-05-30 01:19:40 +0530

    Updates

---
+++ precache-manifest.008012a3416b756097f041f29d120470.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04550625c2e5d3b9ebae3b508a0f346",
+    "url": "/covid19-cases/index.html"
+  },
+  {
+    "revision": "d847e288ee9b9d7e49da",
+    "url": "/covid19-cases/static/css/main.54157282.chunk.css"
+  },
+  {
+    "revision": "cf811214a07ff9062da5",
+    "url": "/covid19-cases/static/js/2.ab048ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-cases/static/js/2.ab048ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d847e288ee9b9d7e49da",
+    "url": "/covid19-cases/static/js/main.15d655c6.chunk.js"
+  },
+  {
+    "revision": "d8029270b992c1187adc",
+    "url": "/covid19-cases/static/js/runtime-main.ccacb7e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19-cases/static/media/logo.5d5d9eef.svg"
+  }
+]);

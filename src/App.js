093bc07b38eba2592448d2304214786ef61923f9
mahesh-chat <maheshchatpatil@gmail.com>
2020-08-06 00:25:46 +0530

    New API added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import CountryList from './components/CountryList/CountryList';
 import SearchBox from './components/SearchBox/SearchBox';
@@ -14,33 +14,37 @@ class App extends React.Component{
   }
   async componentDidMount(){
     const resp = await fetch('https://api.covid19api.com/countries')
-    const countries = await resp.json()
-    this.setState({countries})
-    this.state.countries.forEach(async country => {
-      const resp = await fetch(`https://api.covid19api.com/total/country/${country.Slug}`)
-      const data = await resp.json()
-      if(data.length)
-      this.setState(prevState => (
-        {stats:prevState.stats.concat({...data[data.length - 1],CountryCode:country.ISO2})}
-      ))
+    const Countries = await resp.json()
+    this.setState({countries : Countries})
+    const resp2 = await fetch ('https://api.covid19api.com/summary')
+    const data = await resp2.json()
+    const a=(data.Countries)
+    this.setState({
+      stats : a
     })
+
+  
+
   }
   handleChange = (e) =>{
     this.setState({searchField:e.target.value})
   }
   render(){
+    
     const {stats,searchField} = this.state
+  
     const filteredCountries = stats.filter(country =>(
       country.Country.toLowerCase().includes(searchField.toLowerCase())
     ))
+    console.log(filteredCountries)
     return(
       <div className="App">
-        <h1>Covid19 Stats Web App</h1>
+        <h1 >Covid-19 Stats</h1>
         <SearchBox placeholder="Enter country name ..." handleChange={this.handleChange}/>
         <CountryList stats = {filteredCountries}/>
     </div>
     )
-  }
+  
 }
-
-export default App;
+}
+export default App
